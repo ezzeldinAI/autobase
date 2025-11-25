d137ed7111c3027748236efcd9ca2548e4632d8c
Ezzeldin AI <ezzeldin.coder.ai@gmail.com>
2025-11-25 08:57:18 +0400

    project initialization

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <section className="flex min-h-screen items-center justify-center bg-neutral-100 dark:bg-background">
+      <Button>Click me</Button>
+    </section>
+  );
+}
